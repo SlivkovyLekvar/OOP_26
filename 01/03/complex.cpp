d2e41b7dca1848b022e4736d6f41a10ea6c798ef
Nicol Tomková <tomkonic@fel.cvut.cz>
2026-02-26 14:34:52 +0100

    scitani, odcitani, nasobeni, defined

--- 01/03/complex.cpp
+++ 01/03/complex.cpp
@@ -1,2 +1,15 @@
 #include "complex.h"
 
+Complex Complex::scitani(const Complex &number){
+    return Complex(real + number.real, imag + number.imag);
+};
+
+Complex Complex::odcitani(const Complex &number){
+    return Complex(real - number.real, imag - number.imag);
+};
+
+Complex Complex::nasobeni(const Complex &number){
+    return Complex(real * number.real - imag * number.imag, real * number.imag + imag * number.real);
+};
+
+
--- 01/03/complex.h
+++ 01/03/complex.h
@@ -5,6 +5,43 @@ private:
     double real;
     double imag;
 public:
+    Complex() : real(0), imag(0) {}
+    Complex(double r, double i) : real(r), imag(i) {}
+    
+    Complex scitani(const Complex& number);
+    Complex odcitani(const Complex& number);
+    Complex nasobeni(const Complex& number);
+
+    void SetReal(double r) { real = r; }
+    void SetImag(double i) { imag = i; }
+    double GetReal() { return real; }
+    double GetImag() { return imag; }
+
+    Complex Copy() { return Complex(real, imag);}
+    Complex& operator=(const Complex& other) {
+        if (this != &other) {
+            real = other.real;
+            imag = other.imag;
+        }
+        return *this;
+    }
+    Complex operator+(const Complex& other) { return scitani(other); }
+    Complex operator-(const Complex& other) { return odcitani(other); }
+    Complex operator*(const Complex& other) { return nasobeni(other); }
+    Complex operator+=(const Complex& other) {
+        *this = scitani(other);
+        return *this;
+    }
+    Complex operator-=(const Complex& other) {
+        *this = odcitani(other);
+        return *this;
+    }
+    Complex operator*=(const Complex& other) {
+        *this = nasobeni(other);
+        return *this;
+    }
+
+
 };
 
 
